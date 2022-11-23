18a0d795e7c5de6dfb62014ff62e866a3cf1bb01
Meriéli Manzano <merieli.dev@gmail.com>
2022-11-23 07:59:06 -0300

    test: storeActions insert mock

--- dashboard/tests/unit/store/storeActions.unit.spec.ts
+++ dashboard/tests/unit/store/storeActions.unit.spec.ts
@@ -1,10 +1,38 @@
-import { Context } from '@/interfaces';
+import httpClient from '@/http/index';
 import { actions } from '@/store';
 import { Actions } from '@/store/type-actions';
+import { flushPromises } from '@vue/test-utils';
+import { ActionContext } from 'vuex';
 
 describe('Actions da Store Vuex', () => {
-    const commit = jest.fn() as jest.Mock<Context['commit']>;
-    const dispatch = jest.fn() as jest.Mock<Promise<Context['dispatch']>>;
+    let url = '';
+    let body = {};
+    const mockError = false;
+
+    jest.mock('@/http/index', () => ({
+        login: (_url: string, _body: object) => {
+            return new Promise((resolve) => {
+                if (mockError) throw Error();
+
+                url = _url;
+                body = _body;
+                resolve(true);
+            });
+        },
+    }));
+    const mockHttp = httpClient as jest.Mocked<typeof httpClient>;
+
+    const context: ActionContext<any, any> = {
+        dispatch: jest.fn(),
+        commit: jest.fn(),
+        state: {},
+        getters: {},
+        rootState: {},
+        rootGetters: {},
+    };
+    const commit = jest.fn();
+    // as jest.Mock<Context['commit']>;
+    const dispatch = jest.fn();
 
     describe('🧪 Unidade:', () => {
         test.only('Dado um token de usuário Quando executar o Login na Store Então o usuário deve ser definido como logado e possuir um Token', async () => {
@@ -13,7 +41,13 @@ describe('Actions da Store Vuex', () => {
                 email: 'joaberson@gmail.com',
                 password: 'adaksa123',
             };
-            await actions[Actions.LOGIN_USER]({ commit, dispatch }, user);
+
+            const result = await actions[Actions.LOGIN_USER](context, user);
+            await flushPromises();
+
+            expect(commit).toHaveBeenCalledTimes(1);
+            expect(dispatch).toHaveBeenCalledTimes(1);
+            expect(result).resolves.toEqual('Algo');
             //     expect(store.commit).toHaveBeenCalled()
             //     expect(store.state.userLogged.token).toBe(token)
             //     expect(store.state.isLogged).toBe(true)
